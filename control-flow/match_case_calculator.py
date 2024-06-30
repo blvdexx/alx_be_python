c0e7553f61e0fa32f3e5754b3373a3690548a064
thunderfoxxdev <126731224+thunderfoxxdev@users.noreply.github.com>
2024-06-30 20:40:40 +0100

    fixmatch

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,24 +1,20 @@
-first_nbr = int(input("Enter the first number:"))
-
-second_nbr = int(input("Enter the second number:"))
-
-operation  = input("Choose the operation (+, -, *, /):")
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+operation = input("Choose the operation (+, -, *, /): ")
 
 match operation:
-    case "+":
-        result = first_nbr + second_nbr
-        print(result)
-    case "-":
-        result = first_nbr - second_nbr
-        print(result)
-    case "*":
-        result = first_nbr * second_nbr
-        print(result)
-    case "/":
-        if second_nbr != 0:
-            print(result = first_nbr / second_nbr)
+    case '+':
+        result = num1 + num2
+    case '-':
+        result = num1 - num2
+    case '*':
+        result = num1 * num2
+    case '/':
+        if num2 != 0:
+            result = num1 / num2
         else:
-            print("Cannot divide by zero.")
+            result = "Cannot divide by zero."
+    case _:
+        result = "Invalid operation."
 
-
-
+print(f"The result is {result}.")
