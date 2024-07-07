997b12ccde9b095e300ac43d9989c1ec4f2da48e
thunderfoxxdev <126731224+thunderfoxxdev@users.noreply.github.com>
2024-07-07 22:31:50 +0100

    sh

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -1,11 +1,9 @@
 def display_menu():
-    print("Shopping List Manager")
+    print("\nShopping List Manager")
     print("1. Add Item")
     print("2. Remove Item")
     print("3. View List")
     print("4. Exit")
-
-
 
 def main():
     shopping_list = []
@@ -14,28 +12,26 @@ def main():
         choice = input("Enter your choice: ")
 
         if choice == '1':
-            # Prompt for and add an item
-            item = input("Enter your choice: ")
-            if item:
-                shopping_list.append(item)
-                print(f"{item} was added to your shopping list.")
-            else:
-                print("Invalid input. Please enter a valid item.")
+            item = input("Enter the item to add: ")
+            shopping_list.append(item)
+            print(f"{item} has been added to the list.")
         elif choice == '2':
-            # Prompt for and remove an item
-            item = input("Enter your choice: ")
-            if item in  shopping_list:
-                 shopping_list.remove(item)
-                 print(f"{item} was removed from your shopping list.")
+            if not shopping_list:
+                print("The shopping list is empty.")
             else:
-                print(f"{item} is not in the list.")
-
+                item = input("Enter the item to remove: ")
+                if item in shopping_list:
+                    shopping_list.remove(item)
+                    print(f"{item} has been removed from the list.")
+                else:
+                    print(f"{item} is not in the list.")
         elif choice == '3':
-            # Display the shopping list
-            print("my shopping_list: ")
-            for item in shopping_list:
-                print(item)
-            pass
+            if not shopping_list:
+                print("The shopping list is empty.")
+            else:
+                print("Shopping List:")
+                for index, item in enumerate(shopping_list, start=1):
+                    print(f"{index}. {item}")
         elif choice == '4':
             print("Goodbye!")
             break
